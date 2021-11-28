407e32e9080e2b77bdea5287a085bfa81fb6016f
aAmanHussain <aamanh30@gmail.com>
2021-11-29 02:52:14 +0530

    Added a route handler to handle all the requests in the application

---
+++ src/app/routes/index.js
@@ -0,0 +1,44 @@
+const { HttpStatus } = require('../models');
+const { loggerService } = require('../services');
+
+const { handler: projectsHandler } = require('./projects');
+const { handler: usersHandler } = require('./users');
+const { handler: errorHandler } = require('./error');
+
+const requestHandler = async (req, res) => {
+  let response = null;
+  try {
+    switch (true) {
+      case !req.url.startsWith('/api'):
+        response = {
+          status: HttpStatus.NOT_FOUND,
+          result: null,
+          message: `Route doesn't exist`
+        };
+        break;
+      case req.url.includes('users-service'):
+        response = await usersHandler(req, res);
+        break;
+      case req.url.includes('projects-service'):
+        response = await projectsHandler(req, res);
+        break;
+      default:
+        response = await errorHandler(req, res);
+    }
+  } catch (ex) {
+    const { status, message } = ex;
+    response = {
+      status: status || HttpStatus.BAD_GATEWAY,
+      result: null,
+      message: message || `Something went wrong`
+    };
+  } finally {
+    loggerService.log(response);
+    res.writeHead(response.status, { 'Content-type': 'application/json' });
+    return res.end(JSON.stringify(response));
+  }
+};
+
+module.exports = {
+  requestHandler
+};
