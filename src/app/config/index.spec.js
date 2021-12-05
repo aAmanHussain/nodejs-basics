8322f20abcf16cb05f475baeb6803ce3bcc3b049
aAmanHussain <aamanh30@gmail.com>
2021-12-05 20:16:55 +0530

    Updated the tests for config barrel file

--- src/app/config/index.spec.js
+++ src/app/config/index.spec.js
@@ -1,4 +1,13 @@
-const { PORT, HOSTNAME } = require('./');
+const {
+  PORT,
+  HOSTNAME,
+  SQL_HOST,
+  SQL_PORT,
+  SQL_DB_NAME,
+  MONGO_HOST,
+  MONGO_PORT,
+  MONGO_DB_NAME
+} = require('./');
 
 describe('config barrel file', () => {
   it('should be the same `PORT`', () => {
@@ -7,5 +16,29 @@ describe('config barrel file', () => {
 
   it('should be the same `HOSTNAME`', () => {
     expect(HOSTNAME).toEqual(`127.0.0.1`);
+  });
+
+  it('should be the same `SQL_HOST`', () => {
+    expect(SQL_HOST).toEqual(`127.0.0.1`);
+  });
+
+  it('should be the same `SQL_PORT`', () => {
+    expect(SQL_PORT).toEqual(8083);
+  });
+
+  it('should be the same `SQL_DB_NAME`', () => {
+    expect(SQL_DB_NAME).toEqual(`project-manager`);
+  });
+
+  it('should be the same `MONGO_HOST`', () => {
+    expect(MONGO_HOST).toEqual(`127.0.0.1`);
+  });
+
+  it('should be the same `MONGO_PORT`', () => {
+    expect(MONGO_PORT).toEqual(27017);
+  });
+
+  it('should be the same `MONGO_DB_NAME`', () => {
+    expect(MONGO_DB_NAME).toEqual(`project-manager`);
   });
 });
